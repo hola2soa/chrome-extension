63c9ab96b3df99698ab1daaa1ed699f81599f197
Even <kiki44552002@gmail.com>
2015-12-08 22:30:12 +0800

    set up NODE_ENV = test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,9 @@
 import chai from 'chai';
 
+// For some unknown issue, we need this for setting up NODE_ENV
+// or it will be `undefined` even we set it via package.json
+process.env.NODE_ENV = 'test';
+
 chai.should();
 
 describe('chrome extension', function() {

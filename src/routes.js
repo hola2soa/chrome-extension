36b58c07f1df7ae11fc8db2c2228d27528a57dc0
Even <kiki44552002@gmail.com>
2015-12-24 08:43:39 +0800

    a nice way displaying routes

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,16 @@
+import React   from 'react';
+import {Route} from 'react-router';
+
 import App         from './containers/App';
 import HomePage    from './containers/HomePage';
 import WelcomePage from './containers/WelcomePage';
 
 export default function getRoutes(store) {
 
-  return {
-    component: App,
-    onEnter: App.onEnter(store),
-
-    childRoutes: [
-      {
-        path: '/',
-        component: HomePage,
-        onEnter: HomePage.onEnter(store)
-      },
-
-      {
-        path: '/welcome',
-        component: WelcomePage
-      }
-    ]
-  };
+  return (
+    <Route component={App} onEnter={App.onEnter(store)}>
+      <Route path='/'       component={HomePage} onEnter={HomePage.onEnter(store)}/>
+      <Route path='welcome' component={WelcomePage}/>
+    </Route>
+  );
 }

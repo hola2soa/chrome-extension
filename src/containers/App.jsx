a4a82ffff81cb4ec74a95fcdff2ba6b0dc2a6b94
Even <kiki44552002@gmail.com>
2016-01-03 20:12:24 +0800

    use a better way initializing member data

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,36 +1,13 @@
-import React, {PropTypes} from 'react';
-import {connect}          from 'react-redux';
+import React from 'react';
 
-import Navbar   from '../components/Home/Navbar';
-
-import {getMember} from '../actions/member';
-
-@connect(state => ({
-  member: state.member.store
-}))
 class App extends React.Component {
-  static propTypes = {
-    member: PropTypes.object.isRequired
-  }
-
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    const {member} = this.props;
     return (
       <div>
-        <Navbar member={member}/>
         {this.props.children}
       </div>
     );
   }
-};
-
-App.onEnter = (store) => (nextState, replaceState, callback) => {
-  store.dispatch(getMember());
-  callback();
 };
 
 export default App;

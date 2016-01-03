9e10e77a55de929d786057d9c7c7fcd93d315e1a
Even <kiki44552002@gmail.com>
2016-01-03 20:11:35 +0800

    remove stuff belongs to `register page`

--- src/containers/WelcomePage.jsx
+++ src/containers/WelcomePage.jsx
@@ -1,34 +1,12 @@
-import React, {PropTypes} from 'react';
-import {connect}          from 'react-redux';
-import {pushPath}         from 'redux-simple-router';
+import React from 'react';
 
-import {createMember} from '../actions/member';
-
-import MemberForm from '../components/Welcome/MemberForm';
-
-@connect(state => ({
-}), {
-  createMember,
-  pushPath
-})
 class WelcomePage extends React.Component {
-  static propTypes = {
-    createMember: PropTypes.func.isRequired,
-    pushPath:     PropTypes.func.isRequired,
-  };
-
   render() {
-
     return (
-      <MemberForm onSubmit={this.register}/>
+      <div>
+        Welcome All in One!
+      </div>
     );
-  }
-
-  register = (data) => {
-    const {createMember, pushPath} = this.props;
-    createMember(data).then(() => {
-      pushPath('/');
-    });
   }
 }
 
